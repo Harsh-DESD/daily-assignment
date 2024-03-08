3ca126cf3c10e91192586e8f700240a91de36155
E1_83818_Harsh <soniharsh026@gmail.com>
2024-03-08 13:25:11 -0500

    added Q2

---
+++ assignQ2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+	int a;
+
+	scanf("%d",&a);
+	
+	printf("char=%c\n",a);
+	printf("Decimal=%d\n",a);
+	printf("Octal=%o\n",a);
+	printf("HexaDecimal=%x\n",a);
+
+	return 0;
+
+}
+
