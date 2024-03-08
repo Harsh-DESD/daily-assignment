2ad2eb9df076b2106cc23c932d69dedc1d824628
E1_83818_Harsh <soniharsh026@gmail.com>
2024-03-08 13:40:23 -0500

    added Q3

---
+++ assignQ3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+
+{
+
+	char c;
+
+	scanf("%c",&c);
+
+	printf("Decimal value %d\n",c);
+	printf("Octal value %o\n",c);
+	printf("Hexadecimal value %x\n",c);
+	
+	int a;
+	
+	scanf("%d",&a);
+	printf("ACSII value of char %c\n",a);
+
+	return 0;
+}
