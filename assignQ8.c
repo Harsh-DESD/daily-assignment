ba63768f21a2b3e717f66c6d870e4c0614dcd204
E1_83818_Harsh <soniharsh026@gmail.com>
2024-03-09 05:44:16 -0500

    added Q8

---
+++ assignQ8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+	int a,b,c;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	
+	float k =(a+b+c)/3.0;
+
+	printf("Avg of numbers is : %.2f\n",k);
+
+	return 0;
+
+}
